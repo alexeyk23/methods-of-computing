2e079b3940a304d52bb425738a9e25b0ede12bba
trebushet <kunak94@mail.ru>
2014-10-03 21:02:24 +0400

    p, q наоборот

--- src/methods/Methods.java
+++ src/methods/Methods.java
@@ -29,20 +29,21 @@ public class Methods {
     public static void main(String[] args) 
     {
         int n=8;
-        int k=4;
+        int k=3;
         double[] a = new double[n+1];
         double[] p = new double[n+1];
         double[] q = new double[n+1];
-        double[] r = new double[]{0,2,4,4,9,9,4,4,2};
+        double[] r = new double[]{0,2,4,9,9,4,4,4,2};
         double[] b = new double[n],c= new double[n];
         Arrays.fill(a, 2); a[1]=1; a[n]=1;
         Arrays.fill(b, 1);
         Arrays.fill(c, 1);
         Arrays.fill(p, 1);
         Arrays.fill(q, 1);
-        q[k]=2;
-        p[k+1]=2;
+        q[k+1]=2;
+        p[k]=2;
         //1
+        /*
         for (int i = 1; i <= k-1; i++) 
         {
           b[i]/=a[i]; 
@@ -51,15 +52,16 @@ public class Methods {
           a[i+1]-=b[i]*c[i];
           r[i+1]-=r[i]*c[i];
           c[i]=0;
-          p[n-i]-=p[n-i+1]*b[i];
-          
+          p[i+1]-=p[i]*b[i];
+          if(i==k-2)//пересечение p и c
+              c[i+1]=p[i+1];
           if(i!=k-1)
-              r[k]-=r[i]*p[n-i+1];
-          p[n-i+1]=0;
-          q[n-i]-=q[n-i+1]*b[i];
-          r[k+1]-=r[i]*q[n-i+1];
-          q[n-i+1]=0;
-        }
+            r[k]-=r[i]*p[i];
+          p[i]=0;          
+          q[i+1]-=q[i]*b[i];
+          r[k+1]-=r[i]*q[i];
+          q[i]=0;
+        }*/
         
     for (int i = n; i>=k+2; i--)
         {
@@ -69,27 +71,38 @@ public class Methods {
             a[i-1]-=c[i-1]*b[i-1];
             r[i-1]-=r[i]*c[i-1];
             b[i-1]=0;            
-            q[n-i+2]-=q[n-i+1]*c[i-1];
+            q[i-1]-=q[i]*c[i-1];
+            if(i-1==k+2)
+              b[i-1]=q[i-1];
             if(i!=k+2)
-                r[k+1]-=r[i]*q[n-i+1];
-            q[n-i+1]=0;
-            p[n-i+2]-=p[n-i+1]*c[i-1];
-            r[k]-=r[i]*p[n-i+1];
-            p[n-i+1]=0;
+              r[k+1]-=r[i]*q[i];           
+            q[i]=0;
+            p[i-1]-=p[i]*c[i-1];
+            r[k]-=r[i]*p[i];
+            p[i]=0;
         }
+       /* p[k+1]/=p[k];
+        r[k]/=p[k];
+        p[k]=1;
+        q[k]-=q[k]*p[k+1];
+        r[k+1]-=q[k]*r[k];
+        q[k]=0;
+        r[k+1]/=q[k+1]; 
+        q[k+1]=1;
+        r[k]-=p[k+1]*r[k+1]; 
+        p[k+1]=0;
       
-        p[k]/=p[k+1];
-        r[k]/=p[k+1];
-        p[k+1]=1;
-        q[k]-=q[k+1]*p[k];
-        r[k+1]-=q[k+1]*r[k];
-        q[k+1]=0;
-        r[k+1]/=q[k]; 
-        q[k]=1;
-        r[k]-=p[k]*r[k+1]; 
-        p[k]=0;
-        
-        for (int i = k; i >= 2; i--) 
+      /*  p[n-k]/=p[n-k+1];
+        r[k]/=p[n-k+1];
+        p[n-k+1]=1;
+        q[n-k]-=q[n-k+1]*p[n-k];
+        r[k+1]-=q[n-k+1]*r[k];
+        q[n-k+1]=0;
+        r[k+1]/=q[n-k]; 
+        q[n-k]=1;
+        r[k]-=p[n-k]*r[k+1]; 
+        p[n-k]=0;
+       /* for (int i = k; i >= 2; i--) 
         {
             r[i-1]-=r[i]*b[i-1];
             b[i-1]=0;
@@ -101,7 +114,7 @@ public class Methods {
          r[i+1]-=r[i]*c[i]; c[i]=0;
          r[i+1]/=a[i+1];a[i+1]=1;
         }
-       
+      */ 
         System.out.print(" b= "+Arrays.toString(b)+"\n a= "+Arrays.toString(a)+"\n c= "+Arrays.toString(c)+
                 "\n r="+Arrays.toString(r)+"\n p= "+Arrays.toString(p)+"\n q= "+Arrays.toString(q));
         double we=0;
