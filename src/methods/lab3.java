3bcbba7c399edb71fe2978e7a1d361a15d7eb430
alexeyk23 <kunak94@mail.ru>
2014-11-28 19:51:58 +0400

    back Iteration ischerpiv

--- src/methods/lab3.java
+++ src/methods/lab3.java
@@ -32,9 +32,82 @@ public class lab3
     static double[] xSolve;
     static int countItera=0;
     static int N=4;
-    public  static  void  Solve(double [][] a,double eps,double lam1,double [] x1,int M)
+    public  static  void  Solve(double [][] a,double epsLabmda, double epsX,double lam1,double [] x1,int M)
     {
-        
+        double[] v2 = new double[N],
+                 v1 = new double[N];
+        Arrays.fill(v2, 0);
+        Arrays.fill(v1, 0);
+        v2 = normalise(x1);
+        double[] x2 = new double[N];
+        Arrays.fill(x2, 0);
+        double[][] E = new double[N][N];
+        for (int i = 0; i < N; i++) {
+            E[i][i]=1;
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) 
+            {
+                E[i][j]-=x1[i]*x1[j];
+            }
+        }
+        double[] f = new double[N];
+        Arrays.fill(f, 0);
+        for (int i = 0; i < N; i++) 
+        {
+            for (int j = 0; j < N; j++)
+            {
+                f[i] += E[i][j]*v2[j];
+            }
+        }
+        countItera=1;
+        Haleckiy(a, f, x2);
+        double lambda21=0,
+               lambda22=1/multiply(x2, v2);
+        do
+        {
+            lambda21=lambda22;
+            v1=v2.clone();
+            v2=normalise(x2);
+            Arrays.fill(f, 0);
+            for (int i = 0; i < N; i++) 
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    f[i] += E[i][j]*v2[j];
+                }
+            }
+            Haleckiy(a, f, x2);
+            lambda22=1/multiply(x2, v2);
+            countItera++;
+        }while(countItera<=M && (Math.abs(lambda22-lambda21)>epsLabmda || getDelta(v2, v1)>epsX));        
+        xSolve=v2.clone();
+        lambda=lambda22;     
+    }
+    public static  double getDelta(double [] a, double [] b)
+    {       
+        double max=Math.abs(a[0]-b[0]);
+        for (int i = 1; i < a.length; i++) 
+        {
+            max=Math.max(max,Math.abs(a[i]-b[i]));
+        }
+        return max;
+    }
+    public static double multiply(double[] a, double[] b)
+    {
+        double res=0;
+        for (int i = 0; i < a.length; i++) {            
+            res+=a[i]*b[i];            
+        }
+        return  res;
+    }
+    static double[] normalise(double[] x) {
+        double[] result = new double[N];
+        double norma = Norma(x);
+        for (int i = 0; i < N; i++) {
+            result[i] = x[i] / norma;
+        }
+        return result;
     }
     public static void Haleckiy(double [][]a, double [] f,double [] solution)
     {
